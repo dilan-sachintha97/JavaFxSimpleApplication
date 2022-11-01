026b1fd526d035abb4e86d4306221ce7de571b0d
dilan-sachintha97 <dil97an@gmail.com>
2022-11-01 11:27:03 +0530

    set data into list

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtLanguages;
+    public ListView<String > lstLanguages;
+    public TextField txtSelectedLanguage;
+
+    public void initialize(){
+        setListData();
+    }
+
+    private void setListData(){
+        ObservableList<String> languages =FXCollections.observableArrayList();
+        languages.add("Jaya");
+        languages.add("Python");
+        languages.add("MySQL");
+        languages.add("JavaFX");
+
+        lstLanguages.setItems(languages);
+    }
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
 }
